14133493aefad53d1f695486844f541ff83373cd
Stamy <ivan.t.stamatov@gmail.com>
2021-02-07 21:06:36 +0200

    Updated the way data is compared.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Data-Engineering-Project.Spotify
 A project about taking data from Spotify using their API and saving the data to a DB.
 
-The logis of the program:
+The logic of the program:
 - Establish authentication for Spotify
     - This allows for automated generatioon of tokens for the Spotify API
 - Check if an SQLite table exists and if not, create one
@@ -10,11 +10,6 @@ The logis of the program:
 
 
 To be implemented:
-- Scheduled tasks with the schedule.py
+- Collecting additional data such as Genres, Album, etc.
 - Visualization of the data
 
-
-Tech stack used so far:
-- Spotify's API
-- SQLite
-- DBeaver to view the SQLite data
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #The Great Library of Dependence
-import pandas as pd 
 import requests
 import json
 from datetime import date
@@ -8,50 +7,50 @@ import sqlite3
 import spotipy
 import spotipy.util as util
 import os
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+from wintoast import ToastNotifier
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 
+# === Limitations
+# Spotify stores 50 recent tracks information only.
+# A track currently playing will not be visible in play history until it has completed. 
+# A track must be played for more than 30 seconds to be included in play history
+
+# Spotify tokens expire after an hour
 os.environ['SPOTIPY_CLIENT_ID'] = ''
 os.environ['SPOTIPY_CLIENT_SECRET'] = ''
 os.environ['SPOTIPY_REDIRECT_URI'] = ''
 
-# Authorization Code Flow
-scope = 'user-read-recently-played'
-username = ''
-token = util.prompt_for_user_token(username,scope)
 
 
+def main():
+    f = open("logs.txt", "a")
+    f.write("[0] Script started at: " + datetime.now().strftime("%m-%d %H:%M:%S") + "\n")
 
+    # Authorization Code Flow
+    scope = 'user-read-recently-played'
+    username = 'd3stamy'
+    token = util.prompt_for_user_token(username,scope)
 
-
-if __name__ == "__main__":
     # Headers based on the API's instructions
     headers = {
         "Accept" : "application/json",
         "Content-Type" : "application/json",
         "Authorization" : "Bearer {token}".format(token=token)
     }
-
-# === Limitations
-# Spotify stores 50 recent tracks information only.
-# A track currently playing will not be visible in play history until it has completed. 
-# A track must be played for more than 30 seconds to be included in play history.
+    f.write("[1] Token generated.\n")
 
     # Get the data from the API and store it
     r = requests.get("https://api.spotify.com/v1/me/player/recently-played?limit=40", headers = headers)
 
     data = r.json()
+    f.write("[2] Data collected from Spotify API.\n")
 
-    song_names = []
-    artist_names = []
-    played_at_list = []
-    timestamps = []
-
-
-    # Connect to an SQLite db
+    # Connect to an SQLite 
     conn = sqlite3.connect('myTracks.db')
     cursor = conn.cursor()
-    print("Successfully Connected to SQLite")
 
     # If the table does not exist, create a new one
     cursor.execute("""CREATE TABLE IF NOT EXISTS myTracks (
@@ -62,19 +61,23 @@ if __name__ == "__main__":
         )""")
 
     conn.commit()
-
-    # Get a list of all timestamps currently in the SQLite db
+    
+    # Get a list of all timestamps currently in the SQLite 
     cursor.execute("SELECT time FROM myTracks")
     timestampList = cursor.fetchall()
     newTimeStampStr = ""
     for x in timestampList:
         newTimeStampStr += str(x)
 
+    songsAdded = ""
+    duplicateCount = 0
+    songsAddedCount = 0
 
     for song in data["items"]:
         if str(song["played_at"]) in newTimeStampStr:
             # If the new timestamp is in the list of existing ones, do not add the data
-            print("This song has already been added to the DB.")
+            # print("Duplicate found: " + song["track"]["album"]["artists"][0]["name"] + " - "  + song["track"]["name"])
+            duplicateCount += 1
         else:
             songName = song["track"]["name"]
             artistName = song["track"]["album"]["artists"][0]["name"]
@@ -82,7 +85,37 @@ if __name__ == "__main__":
             datePlayed = song["played_at"][0:10]
             cursor.execute('INSERT INTO myTracks VALUES (?,?,?,?)', (songName, artistName, playedAt, datePlayed))
             conn.commit()
+            print("Added a new one: " + artistName + " - " + songName)
+            songsAdded += artistName + " - " + songName + "\n"
+            songsAddedCount += 1
 
     conn.close()
-    print("End.")
+    f.write("[3] Data compared to existing in SQLite.\n")
 
+    if not songsAdded:
+        # print("There were no new songs.")
+        f.write("[4] There were no new songs.\n")
+    else:
+        # print("songs added are: " + songsAdded)
+        f.write("[4] There were [" + songsAddedCount + "] new songs\n")
+        toaster = ToastNotifier()
+        toaster.show_toast("New songs have been added.",
+                       songsAdded,
+                       icon_path="custom.ico",
+                       duration=5)
+    
+    f.write("[5] Number of duplicates: " + str(duplicateCount) + "\n")
+
+    f.write("[6] Script ended at  : " + datetime.now().strftime("%m-%d %H:%M:%S") + "\n======\n")
+    f.close()
+    return   
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+#scheduler = BlockingScheduler()
+#scheduler.add_job(main, 'interval', minutes = 5)
+#scheduler.start()
